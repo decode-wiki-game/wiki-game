44b4429f023aedaf539dd514a25e0258e9acb1b8
Simon2828 <Simon@Simons-MacBook-Pro.local>
2016-11-16 14:19:26 -0500

    ui update

--- app/assets/js/components/Lobby.jsx
+++ app/assets/js/components/Lobby.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
-
+import Copy from './Copy';
+import LobbyList from './LobbyList';
 
 export default class Lobby extends React.Component{
-	render() {
-		return (
-			<div>      
-	        	<header className="lb-header">
-					<a href="#">rules</a>
-					<h1>Welcome</h1>
-				</header>
-				<main className="lb-main">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio at, laborum, aut culpa qui iusto, velit vero dolore eaque eligendi magnam nesciunt in. Molestias, ab, praesentium. Itaque rem, esse aperiam!
-				</main>
-				<div className="lb-container">
-					<button>start</button>
-					<label className="checkbox"><input type="checkbox" />Make public</label>
-				</div>  
-      		</div>
+		constructor() {
+			super();
+			this.state = {
+				showLobby: false
+			}
+		}
+		onClick() {
+			this.setState({
+				showLobby: !this.state.showLobby
+			})
+		}
+		render() {
+			return (
+				<div>      
+		        	<header className="lb-header">
+						<a href="#">How to play</a>
+						<h1>Welcome to WikiSprint!</h1>
+					</header>
+					<main className="lb-main">
+						<div className="lb-main__div--container">
+							<h3 className="lb-main__h3">You are playing as: (name - click to edit)</h3>
+							<h3 className="lb-main__h3">Send the url and challenge a friend!</h3>
+							<div><small>Click to copy this url and send to a friend</small></div>
+							<Copy />
+							<label className="lb-main__label"><input type="checkbox" />Add game to lobby</label>
+							<h3>Or join a game in the lobby</h3>
+							<button onClick={this.onClick.bind(this)}>lobby</button>
+							<div>
+							{this.state.showLobby ? <LobbyList /> : null}	
+							</div>
+				      	</div>
+					</main>
+					<div className="lb-container">
+						<button>start</button>
+					</div>  
+	      		</div>
 			)
 	}
 }
---
+++ app/assets/js/components/StartButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class StartButton extends React.Component{
+	render() {
+		return (
+			<div>
+				<button className="lb-main__button--red">start wikisprint!</button>
+			</div>
+		)
+	}
+}
