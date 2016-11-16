02eb11f62c1ca968e552928db6e644b6d7125492
Ike Saunders <saunders.ike@gmail.com>
2016-11-16 21:01:07 +0000

    API/front-end integration

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,14 +1,26 @@
 // Routes
 const api = require("../methods/api")
 const routes = require('express').Router();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-// Socket.io
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+routes.use(bodyParser.urlencoded({
+    extended: false
+}), cookieParser());
 
-routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'Connected!' });
+routes.get('/', (request, response) => {
+  response.status(200).json({ message: 'Connected!' });
+});
+
+routes.get('/create', (request, response) => {
+    var playerToken = request.headers['x-usertoken']
+    api.findPlayerFromSessionId(playerToken)
+        .then(player => {
+            api.createGame(player.id)
+                .then(game=> {
+                    response.status(200).json({ game: game });
+                })
+        })
 });
 
 
