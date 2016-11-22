aba06e9e68c7eb095c47cccf344e32a9d4925476
Ike Saunders <saunders.ike@gmail.com>
2016-11-22 23:42:23 +0000

    jsx inline css to hide target from ctrl-f

--- app/assets/js/components/Gamemeta.jsx
+++ app/assets/js/components/Gamemeta.jsx
@@ -3,6 +3,7 @@ var {
     Component,
     PropTypes
 } = React;
+var InlineCss = require('react-inline-css');
 
 export default class Gamemeta extends Component {
     constructor(props) {
@@ -12,9 +13,15 @@ export default class Gamemeta extends Component {
 
     render() {
         return (
+            <InlineCss stylesheet={`
+                & .game-meta__title::after {
+                    content: '${this.state.game.targetTitle}';
+                }
+                `}>
             <div className="game-meta">
-                <h2>Target: <span className="test">{this.state.game.targetTitle}</span></h2>
+                <h2>Target: <span className="game-meta__title"></span></h2>
             </div>
+            </InlineCss>
         )
     }
 }
