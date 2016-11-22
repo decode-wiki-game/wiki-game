bc3de62b6e521bc62170da661d77f534fc22a503
Ike Saunders <saunders.ike@gmail.com>
2016-11-22 23:54:50 +0000

    Improved inline-css methodology

--- app/assets/js/components/Gamemeta.jsx
+++ app/assets/js/components/Gamemeta.jsx
@@ -3,25 +3,25 @@ var {
     Component,
     PropTypes
 } = React;
-var InlineCss = require('react-inline-css');
 
 export default class Gamemeta extends Component {
     constructor(props) {
         super(props);
         this.state = this.props.parent;
     }
+    
+    _createCSS() {
+        console.log('creating CSS!')
+        return {__html: `.game-meta__title::after{content: '${this.state.game.targetTitle}'}`}
+    }
 
     render() {
+        console.log(this._createCSS())
         return (
-            <InlineCss stylesheet={`
-                & .game-meta__title::after {
-                    content: '${this.state.game.targetTitle}';
-                }
-                `}>
             <div className="game-meta">
+            <style dangerouslySetInnerHTML={this._createCSS()} />
                 <h2>Target: <span className="game-meta__title"></span></h2>
             </div>
-            </InlineCss>
         )
     }
 }
